745ae1d3773c897e812f3647eb857ee06f48199d
t.logvinn <dedal_0@mail.ru>
2019-08-20 15:56:52 +0300

    версия 1.3  - начало тестирования веб-страниц, в т.ч. API

--- apps/task_test/tests/models/product_title.py
+++ apps/task_test/tests/models/product_title.py
@@ -9,15 +9,19 @@ class ProductTitleModelsCase(TestCase):
     """
     Первый тест для данного приложения
     """
+    title = None
 
     @classmethod
     def setUpClass(cls):
         print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        ProductTitleModel.objects.create(title='Some City Area or Disctrict')
+        ProductTitleModel.objects.create(title="Some Test Area")
         return super(ProductTitleModelsCase, cls).setUpClass()
 
     def setUp(self):
         print("setUp: Run once for every test method to setup clean data.")
+        some_area = ProductTitleModel.objects.all().first()
+        self.title = some_area._meta.get_field('title')
+        print(self.title)
         pass
 
     def test_title_verbose_name(self):
@@ -25,9 +29,9 @@ class ProductTitleModelsCase(TestCase):
         проверка - в качестве verbose_name задаётся функция gettext_lazy - для дальнейшего перевода
         :return: True - именно она, иначе - False
         """
-        area = ProductTitleModel.objects.all().first()
-        title = area._meta.get_field('title')
-        self.assertTrue(type(title.verbose_name), gettext_lazy)
+        # area = ProductTitleModel.objects.all().first()
+        print(self.title)
+        self.assertTrue(type(self.title.verbose_name), gettext_lazy)
 
     def test_title_max_length(self):
         """
@@ -36,31 +40,33 @@ class ProductTitleModelsCase(TestCase):
         """
         area = ProductTitleModel.objects.all().first()
         title = area._meta.get_field('title')
-        self.assertEqual(title.max_length, 255)
+        self.assertEqual(self.title.max_length, 255)
 
     def test_title_unique(self):
         """
         проверка максимальной длительности названия района, а именно 255
         :return:
         """
-        area = ProductTitleModel.objects.all().first()
-        title = area._meta.get_field('title')
-        self.assertTrue(title.unique)
+        self.assertTrue(self.title.unique)
 
     def test_title_blank(self):
         """
         проверка максимальной длительности названия района, а именно 255
         :return:
         """
-        area = ProductTitleModel.objects.all().first()
-        title = area._meta.get_field('title')
-        self.assertFalse(title.blank)
+        self.assertFalse(self.title.blank)
 
     def test_title_null(self):
         """
         проверка максимальной длительности названия района, а именно 255
         :return:
         """
+        self.assertFalse(self.title.null)
+
+    def test_model_str(self):
+        """
+        Проверка содержимого строки
+        :return:
+        """
         area = ProductTitleModel.objects.all().first()
-        title = area._meta.get_field('title')
-        self.assertFalse(title.null)
+        self.assertEqual(str(area), area.title)
